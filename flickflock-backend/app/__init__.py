e201f527ac197161b31ae77481eac4c2700acb0a
katanujoy <katanujoyy99@gmail.com>
2025-07-22 08:23:47 +0300

    fixed movie routes.py

--- flickflock-backend/app/__init__.py
+++ flickflock-backend/app/__init__.py
@@ -2,13 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_migrate import Migrate
-from flask_cors import CORS
-from flask_restful import Api
-
-
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
-})
 
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate()
@@ -30,19 +23,10 @@ def create_app():
     from .routes.watchlists_routes import WatchlistResource
     from .routes.membership_routes import membership_bp
     from .routes.user_routes import user_bp
-    from .routes.follow_routes import follow_bp
-    from .routes.movie_routes import movies_bp
-    from .routes.report_routes import report_bp
-
-    api.add_resource(ClubResource, '/clubs')
-    api.add_resource(CommentResource, '/comments')
-    api.add_resource(PostResource, '/posts')
-    api.add_resource(RecommendationResource, '/recommendations')
-    api.add_resource(WatchlistResource, '/watchlist')
-    app.register_blueprint(membership_bp)    
     app.register_blueprint(user_bp)
     app.register_blueprint(follow_bp)
     app.register_blueprint(movies_bp)
     app.register_blueprint(report_bp)
+    app.register_blueprint(movies_series_bp)  
 
     return app
--- flickflock-backend/app/routes/movie_routes.py
+++ flickflock-backend/app/routes/movie_routes.py
@@ -1,4 +1,3 @@
-# app/routes/movies_series.py
 from flask import Blueprint, request, jsonify
 from app import db
 from ..models.movie_series import MovieSeries
@@ -11,22 +10,44 @@ movies_bp = Blueprint('movies_bp', __name__, url_prefix='/api/movies')
 @movies_bp.route('', methods=['GET'])
 def get_movies():
     movies = MovieSeries.query.all()
-    return jsonify([movie.title for movie in movies])
+    return jsonify([
+        {
+            "id": movie.id,
+            "title": movie.title,
+            "description": movie.description,
+            "genre": movie.genre,
+            "type": movie.type
+        } for movie in movies
+    ])
 
 @jwt_required
 @movies_bp.route('/<int:id>', methods=['GET'])
 def get_movie(id):
     movie = MovieSeries.query.get_or_404(id)
-    return jsonify({"title": movie.title, "description": movie.description, "type": movie.type})
+    return jsonify({
+        "id": movie.id,
+        "title": movie.title,
+        "description": movie.description,
+        "genre": movie.genre,
+        "type": movie.type
+    })
 
 @jwt_required
 @movies_bp.route('', methods=['POST'])
 def create_movie():
     data = request.get_json()
-    movie = MovieSeries(title=data['title'], description=data.get('description'), genre=data.get('genre'), type=data['type'])
-    db.session.add(movie)
-    db.session.commit()
-    return jsonify({"message": "Movie/Series created"}), 201
+    try:
+        movie = MovieSeries(
+            title=data['title'],
+            description=data.get('description'),
+            genre=data.get('genre'),
+            type=data['type']
+        )
+        db.session.add(movie)
+        db.session.commit()
+        return jsonify({"message": "Movie/Series created", "id": movie.id}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 @jwt_required
 @movies_bp.route('/<int:id>', methods=['PUT'])
@@ -35,6 +56,8 @@ def update_movie(id):
     data = request.get_json()
     movie.title = data.get('title', movie.title)
     movie.description = data.get('description', movie.description)
+    movie.genre = data.get('genre', movie.genre)
+    movie.type = data.get('type', movie.type)
     db.session.commit()
     return jsonify({"message": "Movie/Series updated"})
 
